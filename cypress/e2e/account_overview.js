a5012c5cca971f6f188af87999323f5bb8a86ea8
Kunal-kankariya <kunalkankriya2203@gmail.com>
2024-10-14 22:23:32 +0530

    test: refactored

--- cypress/e2e/account_overview.js
+++ cypress/e2e/account_overview.js
@@ -1,8 +1,8 @@
 const accountVerification = (ele1, ele2, ele3, ele4) => {
-  cy.getText(ele1, ele2).as("accountDetails"); // Alias the expected account ID 
+  cy.getText(ele1, ele2).as("accountDetails"); // Alias the expected account ID
   cy.get("@accountDetails").then((expectedAccountId) => {
-    cy.get('[href^="activity"]').click();
-    cy.contains("CHECKING").should("be.visible");
+    cy.clickOptionWithElement('[href^="activity"]');
+    cy.ensureTextPresent("CHECKING");
     cy.getText(ele3, ele4).as("actualAccountDetails"); // Alias the actual account ID
     cy.get("@actualAccountDetails").then((actualAccountId) => {
       expect(expectedAccountId).to.equal(actualAccountId); // Compare the expected and actual IDs
@@ -11,12 +11,9 @@ const accountVerification = (ele1, ele2, ele3, ele4) => {
 };
 
 describe("Account Overview", () => {
-  before("navigate to account", () => {
+  it("user account validation", () => {
     cy.loginWithCredential();
     cy.ensureTextPresent("Total");
-  });
-
-  it("user account validation", () => {
     accountVerification(1, 0, 0, 1);
     cy.go("back");
     cy.ensureTextPresent("Total");
